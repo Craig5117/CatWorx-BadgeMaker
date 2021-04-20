e67a4d72227416ef7d8b772da819ea5d0bc29943
Craig5117 <craig.bennett5117@gmail.com>
2021-04-20 09:25:16 -0400

    added menu selection to toggle between manual and API input

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -8,8 +8,19 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         {
             List<Employee> employees = new List<Employee>();
-            // employees = PeopleFetcher.GetEmployees();
-            employees = PeopleFetcher.GetFromAPI();
+            Console.Clear();
+            Console.WriteLine("Press 1 then enter to input employee info manually.");
+            Console.WriteLine("Press 2 then enter to retrieve employee info from an API.");
+            string userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "1":
+                    employees = PeopleFetcher.GetEmployees();
+                    break;
+                case "2":
+                    employees = PeopleFetcher.GetFromAPI();
+                    break;
+            }            
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
         }
